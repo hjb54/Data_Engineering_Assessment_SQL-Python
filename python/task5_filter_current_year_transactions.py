b36176cfc340ba1c1d60fc56b1b8c04315a9a34a
Hannah Brown <104782011+hjb54@users.noreply.github.com>
2026-01-12 12:30:45 -0600

    Update task5_filter_current_year_transactions.py

--- python/task5_filter_current_year_transactions.py
+++ python/task5_filter_current_year_transactions.py
@@ -1,1 +1,17 @@
+"""
+Purpose: Yield only transactions occurring in the current year using a generator.
+Concepts: streaming data processing, datetime parsing, safe line-by-line ETL
+"""
 
+import datetime 
+def filter_current_year_transactions(transaction_lines): 
+  current_year = datetime.now().year #get current year as int 
+  for line in transaction_lines: 
+    id, amount, date = line.strip().split(',') #split by comma, remove whitespace, assign strs to corresponding variable 
+    transaction_year = int(date[:4]) #stores the transaction year by pulling index 0-3 of YYYY-MM-DD. as int for easy comparison 
+    if transaction_year == current_year: 
+      yield { # yield dictionary for each transaction 
+        'id': id, 
+        'amount': float(amount), #example output is a float 
+        'date': date 
+      }
