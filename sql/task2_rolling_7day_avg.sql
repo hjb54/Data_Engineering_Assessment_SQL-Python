724c662b60a433260b82fdc79339144331190347
Hannah Brown <104782011+hjb54@users.noreply.github.com>
2026-01-12 12:15:07 -0600

    Update task2_rolling_7day_avg.sql

--- sql/task2_rolling_7day_avg.sql
+++ sql/task2_rolling_7day_avg.sql
@@ -1,1 +1,10 @@
+-- Purpose: Calculate a 7-day running average of prices excluding values > $100
+-- Concepts: window functions, RANGE-based date windowing, conditional aggregation
 
+SELECT product_id, date, price,  
+  AVG(CASE WHEN price <= 100 THEN price END) OVER ( -- only prices under $100 are included in avg 
+    PARTITION BY product_id -- split up by product_id to do individual calculations for dif products 
+    ORDER BY date ASC -- date moves forward in time, makes sure the PRECEDING has the right time window 
+    RANGE BETWEEN INTERVAL '6' DAY PRECEDING AND CURRENT ROW -- goes by date, starts at current day, goes to # days prior, includes all data between those dates.  
+  ) AS running_7days_avg -- final output will have columns: product_id, date, price, and running_7days_avg 
+  FROM daily_prices -- data source
